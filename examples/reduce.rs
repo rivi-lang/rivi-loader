05c92e0c2e08aae295df1f32989131f0cb1e801f
Juuso Haavisto <juuso@mail.com>
2022-01-26 15:16:28 +0000

    update reduce

--- examples/reduce.rs
+++ examples/reduce.rs
@@ -7,7 +7,7 @@ fn main() {
 
     let vk = rivi_loader::new(DebugOption::None).unwrap();
 
-    let mut cursor = std::io::Cursor::new(&include_bytes!("./reduce/out.spv")[..]);
+    let mut cursor = std::io::Cursor::new(&include_bytes!("./reduce/reduce.spv")[..]);
     let shader = vk.load_shader(&mut cursor).unwrap();
 
     vk.compute(input, &mut output, &shader).unwrap();
